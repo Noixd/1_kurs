12efa6029f81b48cddb7455f1a32fac86e9f1a89
Noix <100488924+Noixd@users.noreply.github.com>
2022-03-19 21:13:49 +0300

    Final

--- Lab19/Header.h
+++ Lab19/Header.h
@@ -5,6 +5,19 @@
 #include <stdio.h>
 #include <locale.h>
 
-int Sel_menu(void);
+void Sel_menu(int *pmenu);
+int Data_inp(struct dev_sample *);
+bool Chk_list(int *quan);
+void Table(struct dev_sample*, int quan);
+void Table_str(struct dev_sample*);
+void Pow_dev(struct dev_sample*, int quan);
+void Slv_dev(struct dev_sample*, int quan);
 
+struct dev_sample
+{
+	char name_prod[22];
+	char name_device[20];
+	int freq;
+	float diss_pw;
+};
 #endif
