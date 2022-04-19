e9d9b0f93395c5fcc5e1c228c32ef6db23adf22d
Noix <100488924+Noixd@users.noreply.github.com>
2022-04-19 22:23:57 +0300

    Create Header.h
    Final

---
+++ Lab21(Sort)/Header.h
@@ -0,0 +1,14 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <math.h>
+#include <time.h>
+
+void Sort_bubble(double* a);
+void Sort_option(double* a);
+void mass_val(double* a);
+
+#endif
