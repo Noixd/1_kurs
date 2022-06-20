2fd235ba21a0e001793e6eb3cd52f447c5edbacc
Noix <100488924+Noixd@users.noreply.github.com>
2022-06-20 19:39:36 +0300

    Add tasks 3, 4, 5

--- Lab22(Files)/main.c
+++ Lab22(Files)/main.c
@@ -1,20 +1,23 @@
 #include "Header.h"
 
-int main(void) {
+int main(int argc, char *argv[]) {
 
 	setlocale(LC_ALL, "ru");
 	srand(time(NULL));
 
 	double a[27];
 	int u_sel;
+	FILE *data_file;
 
 	printf("1. Пузырьковая сортировка;\n");
 	printf("2. Сортировка выбором;\n");
 	printf("3. Вывод данных из текстового файла на экран;\n");
+	printf("4. Генерация и запись в бинарный файл;\n");
+	printf("5. Чтение из бинарного файла и вывод max и min значения;\n");
 	printf("0. Выход из программы.\n");
 
 	while (1) {
-		while (scanf("%d", &u_sel)) {
+		while (scanf("%d", &u_sel) != 1) {
 			printf("Неверное введенное значение, попробуйте еще: ");
 			while (getchar() != '\n');
 		}
@@ -34,17 +37,35 @@ int main(void) {
 			break;
 
 		case 3:
-			File_R;
+			File_R();
+			break;
+
+		case 4:
+			mass_val(a);
+			BinaryW_F(a);
+			break;
+
+		case 5:
+			if (argc!=2)
+			{
+				printf("Формат запуска программы\n \"%s file\"\n где file - имя выводимого на экран файла\n", argv[0]);
+				exit(1);
+			}
+
+			if ((data_file = fopen("DATA_binary", "rb")) == NULL)
+			{
+				printf("Невозможно открыть файл %s\n", argv[1]);
+				exit(2);
+			}
+			else
+			{
+				BinaryW_R(a, data_file);
+			}
 			break;
 
 		case 0:
 			printf("Завершение процесса.");
 			return 0;
-
-		default:
-			printf("Неверное введенное значение, попробуйте еще: ");
-			while (getchar() != '\n');
-			break;
 		}
 	}
 }
